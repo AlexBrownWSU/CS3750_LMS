255306a7907950b108f306b383f26c58b98aa737
Alex Brown <alexbrown@alexs-mbp-8.wireless.weber.edu>
2019-10-04 08:43:02 -0600

    Add Address entity

---
+++ src/DAO/Entity/Address.java
@@ -0,0 +1,86 @@
+package DAO.Entity;
+
+public class Address {
+
+    int AddressId;
+    int userId;
+    String lineOne;
+    String lineTwo;
+    String lineThree;
+    String city;
+    int zip;
+    String state;
+    String country;
+
+    public int getAddressId() {
+        return AddressId;
+    }
+
+    public void setAddressId(int addressId) {
+        AddressId = addressId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getLineOne() {
+        return lineOne;
+    }
+
+    public void setLineOne(String lineOne) {
+        this.lineOne = lineOne;
+    }
+
+    public String getLineTwo() {
+        return lineTwo;
+    }
+
+    public void setLineTwo(String lineTwo) {
+        this.lineTwo = lineTwo;
+    }
+
+    public String getLineThree() {
+        return lineThree;
+    }
+
+    public void setLineThree(String lineThree) {
+        this.lineThree = lineThree;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
