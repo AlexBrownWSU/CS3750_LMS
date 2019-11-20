ade223dad2a4ef8e98b829ebed8eadcdaf5e2d7e
Logan <logantcooper6@gmail.com>
2019-11-20 15:38:20 -0700

    Calendar and Servlet

--- src/webapp/getCalendar.java
+++ src/webapp/getCalendar.java
@@ -14,14 +14,14 @@ import java.util.List;
 
 @WebServlet(name = "getCalendar")
 public class getCalendar extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int studentId = request.getParameter("studentId");
         List<Assignment> assignments = GetStudentAssignments.getAssignmentByStudentId(studentId);
         request.setAttribute("assignments", assignments);
-        request.getRequestDispatcher("/calendar.jsp").forward(request, response);
+        request.getRequestDispatcher("/fullcalendar.jsp").forward(request, response);
     }
 }
